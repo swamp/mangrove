06be0e649bda6fea5c7f8eb9e63719776e09e328
peter.bjorklund <piot@hotmail.com>
2024-12-04 14:58:45 +0100

    chore: header

--- crates/save-detector/src/lib.rs
+++ crates/save-detector/src/lib.rs
@@ -2,7 +2,6 @@
  * Copyright (c) Peter Bjorklund. All rights reserved. https://github.com/swamp/mangrove
  * Licensed under the MIT License. See LICENSE in the project root for license information.
  */
-
 use limnus_message::prelude::Message;
 use mangrove_script::ScriptMessage;
 use message_channel::{Channel, Receiver};
