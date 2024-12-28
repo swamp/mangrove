0ef61e21e6cee1d6ca3451c31647577a5f1f5e11
Peter Bjorklund <piot@hotmail.com>
2024-12-28 16:32:12 +0100

    fix: adjust resolution to 320x180

--- crates/mangrove/src/main.rs
+++ crates/mangrove/src/main.rs
@@ -12,12 +12,15 @@ use mangrove_script::ScriptPlugin;
 use swamp::prelude::SwampDefaultPlugins;
 use swamp::prelude::*;
 
+//const RESOLUTION: (u16, u16) = (640, 360);
+const RESOLUTION: (u16, u16) = (640 / 2, 360 / 2);
+
 fn main() {
     App::new()
         .insert_resource(Window {
             title: "mangrove".to_string(),
-            requested_surface_size: UVec2::new(320 * 6, 240 * 6),
-            minimal_surface_size: UVec2::new(320, 240),
+            requested_surface_size: UVec2::new(RESOLUTION.0 * 6, RESOLUTION.1 * 6),
+            minimal_surface_size: UVec2::new(RESOLUTION.0, RESOLUTION.1),
             fullscreen: false,
         })
         .add_plugins(DefaultPlugins)
