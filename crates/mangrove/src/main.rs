f26846ce9ab2d1277bc4f9f54829e82de1113cd0
Peter Bjorklund <piot@hotmail.com>
2025-01-13 20:22:42 +0100

    fix: add resolutions

--- crates/mangrove/src/main.rs
+++ crates/mangrove/src/main.rs
@@ -11,13 +11,20 @@ use mangrove_script::ScriptPlugin;
 use swamp::prelude::SwampDefaultPlugins;
 use swamp::prelude::*;
 
-const RESOLUTION: (u16, u16) = (640 / 2, 360 / 2);
+const OLD_SCHOOL_RESOLUTION_LOW: (u16, u16) = (320, 180);
+#[allow(dead_code)]
+const OLD_SCHOOL_RESOLUTION_HIGH: (u16, u16) = (OLD_SCHOOL_RESOLUTION_LOW.0 * 2, OLD_SCHOOL_RESOLUTION_LOW.1 * 2);
+#[allow(dead_code)]
+const INTERMEDIATE_RESOLUTION: (u16, u16) = (384, 216);
+
+const RESOLUTION: (u16, u16) = OLD_SCHOOL_RESOLUTION_LOW;
+const FACTOR: u16 = 5;
 
 fn main() {
     App::new()
         .insert_resource(Window {
             title: "mangrove".to_string(),
-            requested_surface_size: UVec2::new(RESOLUTION.0 * 6, RESOLUTION.1 * 6),
+            requested_surface_size: UVec2::new(RESOLUTION.0 * FACTOR, RESOLUTION.1 * FACTOR),
             minimal_surface_size: UVec2::new(RESOLUTION.0, RESOLUTION.1),
             mode: ScreenMode::WindowedOnTop,
         })
